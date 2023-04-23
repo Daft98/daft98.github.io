8af8800cc8167aa2476b37c4e5f6a4363ad5346c
Dennis Fernández <daft98@hotmail.com>
2023-04-23 17:23:52 -0500

    new port

--- web-page/Page2/script.js
+++ web-page/Page2/script.js
@@ -5,14 +5,14 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 };
-const sr=ScrollReveal({
+const sr = ScrollReveal({
     distance: '65px',
     duration: 2600,
-    delay:450,
+    delay: 450,
     reset: true
 });
 
-sr.reveal('.hero-text',{delay:200, origin:'top'});
-sr.reveal('.hero-img',{delay:450, origin:'top'});
-sr.reveal('.icons',{delay:500, origin:'top'});
-sr.reveal('.scroll-down',{delay:200, origin:'rigth'});
+sr.reveal('.hero-text', { delay: 200, origin: 'top' });
+sr.reveal('.hero-img', { delay: 450, origin: 'top' });
+sr.reveal('.icons', { delay: 500, origin: 'left' });
+sr.reveal('.scroll-down', { delay: 200, origin: 'rigth' });
